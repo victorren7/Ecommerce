e8e74daa80d31d94e56e0f09696b5a26dd422554
victor <31532226+victorren7@users.noreply.github.com>
2020-08-07 11:49:01 -0700

    created backend

---
+++ backend/data.js
@@ -0,0 +1,34 @@
+export default {
+  products: [
+  {
+    _id: '1',
+    name: 'Slim shirt',
+    category: 'shirts',
+    image: '/images/nike-shirt.jpg',
+    price: 60,
+    brand: 'Nike',
+    rating: 4.5,
+    numReviews: 10
+  },
+  {
+    _id: '2',
+    name: 'Fit shirt',
+    category: 'shirts',
+    image: '/images/nike-shirt.jpg',
+    price: 50,
+    brand: 'Nike',
+    rating: 5,
+    numReviews: 10
+  },
+  {
+    _id: '3',
+    name: 'Slim pants',
+    image: '/images/nike-shirt.jpg',
+    category: 'pants',
+    price: 70,
+    brand: 'Nike',
+    rating: 3.5,
+    numReviews: 10
+  },
+]
+}
